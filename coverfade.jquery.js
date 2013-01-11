e12a653ae1f1a0abc045f401e58bd37f2197b2aa
Martin <contact@martindunn.net>
2013-01-11 23:03:24 +0000

    Created coverfade plugin file and used .html instead of .php

--- coverfade.jquery.js
+++ coverfade.jquery.js
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-<head>
-<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
-<script type="text/javascript">
-
 /* coverfade plugin */
 (function ($) {
 	
@@ -83,34 +76,3 @@
 		
 	}	
 })(jQuery);
-	
-$(document).ready(function () {
-
-	$.background(['/img/Penguins.jpg', '/img/Desert.jpg', '/img/Chrysanthemum.jpg', '/img/Hydrangeas.jpg', '/img/Jellyfish.jpg', '/img/Koala.jpg', '/img/Lighthouse.jpg', '/img/Penguins.jpg', '/img/Tulips.jpg'], 5000);
-		
-});
-</script>
-<style>
-	body {
-		font-family:Arial, Helvetica, sans-serif
-	}
-	.container {
-		margin:0 auto;
-		width:60%;
-		background-color:#fff;
-		opacity:0.8;
-		z-index:1;
-		padding:5%;
-		color:#333;
-	}			
-</style>
-</head>
-<body>
-    <div class="container">
-    	<h1>This is my new website!</h1>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis sem libero, id congue nisl. Vivamus ac sapien sed nulla consectetur aliquam eu ultricies lorem. Quisque et sem enim. Quisque lacinia libero at ligula consectetur a viverra magna gravida. Aenean suscipit accumsan erat. Nullam eget libero augue. Vestibulum pellentesque dolor vitae risus lobortis non condimentum purus accumsan. Suspendisse congue nunc eget tortor ornare molestie ac ac dui.</p>
-
-		<p>In orci nunc, interdum quis placerat nec, posuere condimentum lorem. Vestibulum sit amet congue libero. Ut ornare nibh lectus, ut semper eros. Cras hendrerit, quam sed cursus mattis, sem risus congue leo, nec luctus est augue et est. Quisque posuere, tortor sed rhoncus ultricies, orci arcu mollis massa, vitae pretium orci lorem quis erat. Proin gravida purus a ligula tempus a facilisis lacus tristique. Ut imperdiet condimentum nisi vitae iaculis. Fusce nunc neque, accumsan quis vulputate in, feugiat non lacus. </p>
-    </div>
-</body>
-</html>
